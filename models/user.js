11f17be6d1739c7291d4e9b7ea78eff8df1da364
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-04-23 17:06:49 -0700

    user should be able to login now

--- models/user.js
+++ models/user.js
@@ -4,10 +4,14 @@
 
 
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
