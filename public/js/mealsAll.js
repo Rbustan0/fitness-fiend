a52bc8d2924c118c2cac7fc9cbd93156dc08ab00
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-04-24 00:21:59 -0700

    made the meal input disply

--- public/js/mealsAll.js
+++ public/js/mealsAll.js
@@ -1,11 +1,22 @@
- 
-    const mealName = document.getElementById('mealName').value
-    const mealDescription = document.getElementById('mealDescription').value
-    const mealCalories = document.getElementById('mealCalories').value
-    const mealType = document.getElementById('mealType').value
-    const newMealBtn = document.getElementById('newMealBtn')
-    const renderInfo = document.getElementById('renderInfo')
+const newMealBtn = document.getElementById('newMealBtn');
+const renderInfo = document.getElementById('renderInfo');
 
+function renderMeal(event) {
+  event.preventDefault();
+  const mealName = document.getElementById('mealName').value;
+  const mealDescription = document.getElementById('mealDescription').value;
+  const mealCalories = document.getElementById('mealCalories').value;
+  const mealType = document.getElementById('mealType').value
+  const selectedValue = mealType;
+  
+  renderInfo.innerHTML = `
+    <h1>Meal Name:</h1><p>${mealName}</p>
+    <h1>Meal Description:</h1><p>${mealDescription}</p>
+    <h1>Meal Type:</h1><p>${selectedValue}</p>
+    <h1>Meal Calories:</h1><p>${mealCalories}</p>`;
+}
+
+newMealBtn.addEventListener('click', renderMeal);
 
    
 
@@ -33,21 +44,3 @@
 //     }
 //   };
   
-  const selectedValue = mealType.value;
-
-  function renderMeal(event){
-    event.preventDefault()
-    renderInfo.innerHTML = `
-    <h1>Meal Name:</h1 <p>${mealName}</p>
-
-    <h1>Meal Description:</h1 <p>${mealDescription}</p>
-
-    <h1>Meal Type:</h1 <p>${selectedValue}</p>
-
-    <h1>Meal Calories:</h1 <p>${mealCalories}</p>`
-
-
-  }
-
-
-  newMealBtn.addEventListener('click', renderMeal)
